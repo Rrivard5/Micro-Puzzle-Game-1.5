df492db443fe450578a7d0d0f042ff0de9768884
Rrivard5 <rebecca.rivard@villanova.edu>
2025-07-14 09:31:03 -0400

    Create App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,52 @@
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Home from './pages/Home'
+import StudentInfo from './pages/StudentInfo'
+import LabRoom from './pages/LabRoom'
+import Completion from './pages/Completion'
+import WordScramble from './pages/WordScramble'
+import InstructorInterface from './components/InstructorInterface'
+import { GameProvider } from './context/GameStateContext'
+import Header from './components/Header'
+import ProtectedRoute from './components/ProtectedRoute'
+
+export default function App() {
+  return (
+    <GameProvider>
+      <Router>
+        <div className="min-h-screen bg-gradient-to-br from-blue-50 via-white to-green-50">
+          <Header />
+          <main className="container mx-auto px-4 py-8">
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/student-info" element={<StudentInfo />} />
+              
+              {/* Protected Lab Route */}
+              <Route 
+                path="/lab" 
+                element={
+                  <ProtectedRoute>
+                    <LabRoom />
+                  </ProtectedRoute>
+                } 
+              />
+              <Route 
+                path="/complete" 
+                element={
+                  <ProtectedRoute>
+                    <Completion />
+                  </ProtectedRoute>
+                } 
+              />
+              
+              {/* Word Scramble can be accessed without student info */}
+              <Route path="/word-scramble" element={<WordScramble />} />
+              
+              {/* Instructor Interface */}
+              <Route path="/instructor" element={<InstructorInterface />} />
+            </Routes>
+          </main>
+        </div>
+      </Router>
+    </GameProvider>
+  )
+}
